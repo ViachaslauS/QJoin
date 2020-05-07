3f5dcc2b695efddc17c4ea4c890f60b52e8f3139
homelessinlaw <43885584+homelessinlaw@users.noreply.github.com>
2020-05-07 12:29:15 +0300

    Add migrations

---
+++ Server1/Server2/Migrations/202005070902065_AddBlogUrl.cs
@@ -0,0 +1,33 @@
+﻿namespace QueueJoin.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBlogUrl : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "dbo.Queues", newName: "Groups");
+            CreateTable(
+                "dbo.QueueJs",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Groups_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Groups", t => t.Groups_Id)
+                .Index(t => t.Groups_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.QueueJs", "Groups_Id", "dbo.Groups");
+            DropIndex("dbo.QueueJs", new[] { "Groups_Id" });
+            DropTable("dbo.QueueJs");
+            RenameTable(name: "dbo.Groups", newName: "Queues");
+        }
+    }
+}
